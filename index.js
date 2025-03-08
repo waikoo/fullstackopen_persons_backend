24373b4a862f23ce8b381856553653347dbecbb5
waikoo <beroer@protonmail.com>
2025-03-08 17:34:18 +0100

    Add: 3.13 - 3.14

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const Entry = require('./models/phonebook.js')
+
 const app = express()
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+// let persons = [
+//   {
+//     "id": "1",
+//     "name": "Arto Hellas",
+//     "number": "040-123456"
+//   },
+//   {
+//     "id": "2",
+//     "name": "Ada Lovelace",
+//     "number": "39-44-5323523"
+//   },
+//   {
+//     "id": "3",
+//     "name": "Dan Abramov",
+//     "number": "12-43-234345"
+//   },
+//   {
+//     "id": "4",
+//     "name": "Mary Poppendieck",
+//     "number": "39-23-6423122"
+//   }
+// ]
 
 app.use(express.static('dist'))
 
@@ -45,7 +48,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Entry.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -76,29 +81,24 @@ app.delete('/api/persons/:id', (req, res) => {
   res.sendStatus(204).end()
 })
 
-const getRandomId = () => {
-  return Math.floor(Math.random() * 1000000)
-}
-
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body.name || !body.number || persons.find(person => person.name === body.name)) {
-    res.send({ error: 'name must be unique' }).sendStatus(400).end()
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'missing name or number', })
   }
 
-  const newPerson = {
+  const newPerson = new Entry({
     name: body.name,
     number: body.number,
-    id: getRandomId()
-  }
+  })
 
-  persons = [...persons, newPerson]
-
-  res.json(newPerson)
+  newPerson.save().then(savedEntry => {
+    res.json(savedEntry)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
